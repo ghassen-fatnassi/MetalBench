dc75a7b7f1b6a35ab364572d4805794a7ef8d6d5
ITSGASS <140975032+ghassen-fatnassi@users.noreply.github.com>
2025-12-06 20:04:04 +0000

    wip

--- custom/example/custom_op.h
+++ custom/example/custom_op.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 
-struct SimpleReLUAddOpKernel;  // forward declare
+struct SimpleReLUAddOpKernel;
 
+// Provide the full struct definition
+struct SimpleReLUAddOpKernel {
+    SimpleReLUAddOpKernel(const OrtApi& api, const OrtKernelInfo* info) : api_(api) {}
+    void Compute(OrtKernelContext* context);
+
+private:
+    const OrtApi& api_;
+};
 struct SimpleReLUAddOp : Ort::CustomOpBase<SimpleReLUAddOp, SimpleReLUAddOpKernel> {
     void* CreateKernel(const OrtApi& api, const OrtKernelInfo* info) const;
     const char* GetName() const { return "SimpleReLUAdd"; }
