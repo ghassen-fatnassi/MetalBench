732593747c37124e83441fde4ff9dc21091a7d48
ITSGASS <140975032+ghassen-fatnassi@users.noreply.github.com>
2025-12-06 21:15:45 +0000

    wip

--- custom/example/simple_relu_add_kernel.cuh
+++
@@ -1,6 +0,0 @@
-// simple_relu_add_kernel.cuh
-#pragma once
-#include <cstddef>   // for size_t
-
-// CUDA kernel
-__global__ void SimpleReLUAddKernel(const float* input1, const float* input2, float* output, size_t size);
