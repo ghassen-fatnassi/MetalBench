80c3b8e97fde35feef6044b817190b6d0aac271e
ghassen-fatnassi <oneinbillion77@gmail.com>
2025-12-10 23:52:58 +0100

    wip

--- Bench/single_run_tensorrt_static.cpp
+++ Bench/single_run_tensorrt_static.cpp
@@ -24,7 +24,7 @@ int main() {
     try {
         OrtCUDAProviderOptions cuda_options{};
         cuda_options.device_id = 0;
-        session_options.AppendExecutionProvider_Tensorrt(cuda_options);
+        Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_Tensorrt(session_options, device_id));
         std::cout << "CUDA Provider Appended.\n";
     } catch (...) {
         std::cerr << "WARNING: Could not append CUDA provider.\n";
