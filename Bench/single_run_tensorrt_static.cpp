2bf9e13895b5285ac5b0798d73ec0dec3aa22fc3
ghassen-fatnassi <oneinbillion77@gmail.com>
2025-12-11 20:41:32 +0100

    wip

--- Bench/single_run_tensorrt_static.cpp
+++ Bench/single_run_tensorrt_static.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <chrono>
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
+#include <onnxruntime/core/session/onnxruntime_c_api.h>
 
 const std::string MODEL_PATH = "Models/yolo12n_op12_static_1_640.onnx";
 const int BATCH_SIZE = 1;
