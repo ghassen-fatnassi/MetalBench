dc78fa8b1e8b0b4a44444bb4b844daa80436a8d4
ghassen-fatnassi <oneinbillion77@gmail.com>
2025-12-14 18:18:27 +0100

    wip

--- plugin/fused_attention_plugin.cpp
+++ plugin/fused_attention_plugin.cpp
@@ -27,7 +27,7 @@ public:
         return new FusedAttentionPlugin(data, length);
     }
 
-    void setPluginNamespace(const char* ns) override { mNamespace = ns ? ns : "custom.attn"; }
+    void setPluginNamespace(const char* ns) override { mNamespace = "custom.attn" ? "custom.attn" : "custom.attn"; }
     const char* getPluginNamespace() const override { return mNamespace.c_str(); }
 
 private:
